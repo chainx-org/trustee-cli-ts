5a0f1e732557f463595e39c821f1d0aa0e89ee89
qinghuan-deep <1245816264@qq.com>
2020-12-15 12:31:47 +0800

    fix

--- src/multisign/ledger.ts
+++ src/multisign/ledger.ts
@@ -47,10 +47,13 @@ class Ledger {
     }
 
     public constructTxObj(raw, inputArr, redeemScript) {
+        console.log('construct .......1')
 
         const net = bitcore.Networks.mainnet;
+        console.log('construct .......2')
 
         const txObj = bitcore.Transaction();
+        console.log('construct .......3')
 
         const utxos = inputArr.map(input => {
             const tx = bitcore.Transaction(input.raw);
@@ -63,19 +66,21 @@ class Ledger {
                 satoshis: input.satoshi
             };
         });
+        console.log('construct .......4')
 
 
         const script = bitcore.Script.fromString(redeemScript);
         const chunks = script.chunks.slice(1, script.chunks.length - 2);
         const pubkeys = chunks.map(chunk => chunk.buf.toString("hex"));
         const m = script.chunks[0].opcodenum - 80;
+        console.log('construct .......5')
 
         try {
             for (let utxo of utxos) {
                 txObj.from(utxo, pubkeys, m, false, { noSorting: true });
             }
         } catch (err) {
-            console.log('construct error.......' + JSON.stringify(err))
+            console.log('construct .......' + JSON.stringify(err))
         }
         const originTx = bitcore.Transaction(raw);
         for (const output of originTx.outputs) {
@@ -83,6 +88,7 @@ class Ledger {
 
             txObj.to(address.toString(), output.satoshis);
         }
+        console.log('construct .......6')
         console.log('redeem script:' + JSON.stringify(redeemScript))
         this.applyAlreadyExistedSig(txObj, raw);
 
@@ -90,8 +96,10 @@ class Ledger {
     }
 
     public applyAlreadyExistedSig(txObj, raw) {
+        console.log('apply .......1')
         const tx = bitcoinjs.Transaction.fromHex(raw);
         const txb = bitcoinjs.TransactionBuilder.fromTransaction(tx, bitcore.Networks.mainnet);
+        console.log('apply .......2')
         txb.__inputs.forEach((input, index) => {
             if (!input.pubkeys) {
                 return;
@@ -116,32 +124,40 @@ class Ledger {
                 txObj.applySignature(obj);
             });
         });
+        console.log('apply .......3')
     }
 
     public async sign(raw, inputsObj, redeemScript, network = "mainnet") {
+        console.log('sign .......1')
         const pubkey = await this.getPubKeyFromLedger(this.appBtc);
         if (!redeemScript) {
             redeemScript = getRedeemScriptFromRaw(raw, this.network);
         }
+        console.log('sign .......2')
         if (!redeemScript) {
             throw new Error("redeem script not provided");
         }
+        console.log('sign .......3')
         const toSignInputs = inputsObj.map(({ raw, index }) => {
             const tx = this.appBtc.splitTransaction(raw, true);
             return [tx, index, redeemScript];
         });
+        console.log('sign .......4')
         const outputScript = this.appBtc
             .serializeTransactionOutputs(this.appBtc.splitTransaction(raw))
             .toString("hex");
 
+        console.log('sign .......5')
         const path = this.network === "mainnet" ? mainnetPath : testnetPath;
         const paths = toSignInputs.map(() => path);
+        console.log('sign .......6')
 
         const result = await this.appBtc.signP2SHTransaction({
             inputs: toSignInputs,
             associatedKeysets: paths,
             outputScriptHex: outputScript
         });
+        console.log('sign .......7')
 
 
         const signatureObjs = result.map(function (sig, index) {
@@ -155,10 +171,12 @@ class Ledger {
                 })
             };
         });
+        console.log('sign .......8')
         const finalTx = this.constructTxObj(raw, inputsObj, redeemScript);
         for (const signature of signatureObjs) {
             finalTx.applySignature(signature);
         }
+        console.log('sign .......9')
         console.log(`this.network: ${this.network}  network ${network} \n`)
         return finalTx.toString('hex');
     }
